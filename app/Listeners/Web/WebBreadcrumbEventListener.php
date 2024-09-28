2e8e7fafa80449077f88cad4e1a9c152f1d859aa
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2024-09-28 07:41:34 +0400

    Fix breadcrumb event listener

--- app/Listeners/Web/WebBreadcrumbEventListener.php
+++ app/Listeners/Web/WebBreadcrumbEventListener.php
@@ -18,23 +18,23 @@ class WebBreadcrumbEventListener
         if ($event->current instanceof Model) {
             $breadcrumb = app_instance('breadcrumb');
 
-            if ($breadcrumb instanceof Collection
-                && ! ($parent = $breadcrumb->last()) instanceof $event->current
-            ) {
-                $event->current->slug = $parent->slug . '/' . $event->current->slug;
+            if ($breadcrumb instanceof Collection) {
+                if (! ($parent = $breadcrumb->last()) instanceof $event->current) {
+                    $event->current->slug = $parent->slug . '/' . $event->current->slug;
 
-                $breadcrumb->push($event->current);
+                    $breadcrumb->push($event->current);
 
-                if (! is_null($event->current->tab_slug)
-                    && ! is_null($event->current->tab_title)
-                ) {
-                    $current = $event->current->newInstance();
+                    if (! is_null($event->current->tab_slug)
+                        && ! is_null($event->current->tab_title)
+                    ) {
+                        $current = $event->current->newInstance();
 
-                    $current->slug = $event->current->tab_slug;
+                        $current->slug = $event->current->tab_slug;
 
-                    $current->title = $event->current->tab_title;
+                        $current->title = $event->current->tab_title;
 
-                    $breadcrumb->push($current);
+                        $breadcrumb->push($current);
+                    }
                 }
             } else {
                 app()->instance('breadcrumb', new Collection([$event->current]));
