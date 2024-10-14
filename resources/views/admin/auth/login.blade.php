bfbb28df4114f06b471dcd4da6085bff59d50468
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2024-10-14 09:02:55 +0400

    Update author head meta tag

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="cms">
-    <meta name="author" content="David Gvritishvili <gvritishvili.david@gmail.com>">
+    <meta name="author" content="Davit Gvritishvili <gvritishvili.davit@gmail.com>">
     <meta name="robots" content="noindex, nofollow">
     <meta name="version" content="{{cms_config('version')}}">
     <title>CMS - Login</title>
