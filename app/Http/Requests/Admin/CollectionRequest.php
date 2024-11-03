b4fd60b7bef5a70cbe38708b2a981aa62b9a1698
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2024-11-03 13:32:14 +0400

    Remove min:1 validation rule when required is present

--- app/Http/Requests/Admin/CollectionRequest.php
+++ app/Http/Requests/Admin/CollectionRequest.php
@@ -18,10 +18,10 @@ class CollectionRequest extends Request
             'type' => 'required',
             'admin_order_by' => 'required',
             'admin_sort' => 'required',
-            'admin_per_page' => 'required|numeric|min:1|max:50',
+            'admin_per_page' => 'required|numeric|max:50',
             'web_order_by' => 'required',
             'web_sort' => 'required',
-            'web_per_page' => 'required|numeric|min:1|max:50'
+            'web_per_page' => 'required|numeric|max:50'
         ];
     }
 
