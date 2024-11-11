4e1148aaab8571af9d355af83da806d6b6cb16fc
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2024-11-11 15:00:54 +0400

    Update routes web file

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Controllers\Web\WebGlideServerController;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Web')->middleware('web.viewData')->group(function ($router) {
     // web routes
-
-    // glide server for image manipulation
-    // $router->get(
-    //     config('web.glide_base_url') . '/{path}',
-    //     [WebGlideServerController::class, 'show']
-    // )->name('glide')->where('path', '.+');
 });
