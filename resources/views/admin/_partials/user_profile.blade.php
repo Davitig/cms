56e3e089e37318731720f1f670c23d88c8d7d9c3
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2024-11-21 00:14:49 +0400

    Split admin menu views

---
+++ resources/views/admin/_partials/user_profile.blade.php
@@ -0,0 +1,50 @@
+<li>
+    <form method="post" action="{{cms_route('lockscreen.put')}}" id="set-lockscreen">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="_method" value="put">
+        <button type="submit" class="btn btn-link" title="Lockscreen">
+            <i class="fa fa-lock"></i>
+        </button>
+    </form>
+</li>
+<li>
+    <a href="{{web_url('/')}}" target="_blank" title="Go to website"><i class="fa fa-desktop"></i></a>
+</li>
+<li class="dropdown user-profile">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <img src="{{ auth('cms')->user()->photo }}" alt="user-image" class="img-circle img-inline userpic-32" width="28">
+        <span>
+            {{auth('cms')->user()->first_name}} {{auth('cms')->user()->last_name}}
+            <i class="fa fa-angle-down"></i>
+        </span>
+    </a>
+    <ul class="dropdown-menu user-profile-menu list-unstyled">
+        <li>
+            <a href="{{cms_route('cmsUsers.show', [$userId = auth('cms')->id()])}}">
+                <i class="{{icon_type('cmsUsers')}}"></i>
+                Profile
+            </a>
+        </li>
+        <li>
+            <a href="{{cms_route('cmsUsers.edit', [$userId])}}">
+                <i class="fa fa-edit"></i>
+                Edit
+            </a>
+        </li>
+        <li>
+            <a href="#help">
+                <i class="fa fa-info"></i>
+                Help
+            </a>
+        </li>
+        <li class="last">
+            <form action="{{cms_route('logout')}}" method="post">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <button>
+                    <i class="fa fa-sign-out"></i>
+                    Logout
+                </button>
+            </form>
+        </li>
+    </ul>
+</li>
